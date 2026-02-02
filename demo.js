707cee9da75afec78098fe3f620faeb6e30364cb
Diksha Saraswat <diksha.saraswat_cs23@gla.ac.in>
2026-02-02 14:36:27 +0530

    commit

---
+++ demo.js
@@ -0,0 +1,8 @@
+const fs =require('fs');
+fs.writeFileSync('example.txt','this is an example file created using Node.js',
+function(e){
+    if(e) console.log("ye aaya error");
+    console.log("done");
+});
+console.log('file created successfully');
+console.log('File system operation completed');
